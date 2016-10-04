ae2df591c5577ca549712e749d935b3b81f23a19
anhta <tatuananh07b3@gmail.com>
2016-10-04 17:55:45 +0700

    commit code add work data access

---
+++ src/main/java/anhta/tutorial/spring/guides/schedule/ScheduledTask.java
@@ -0,0 +1,23 @@
+package anhta.tutorial.spring.guides.schedule;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTask {
+	
+	private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
+	
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	
+	@Scheduled(fixedRate = 2000)
+	public void reportCurrentTime() {
+		log.info("This time is now {}", dateFormat.format(new Date()));
+	}
+	
+}
